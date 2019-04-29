f82e045a15e3718e43ecc521a1206dca7528332a
Zachary Williams <zwilliams.dev@gmail.com>
2019-04-29 01:40:39 -0400

    Setup react routes and brought in materialize-css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+// All reducers in /reducers/index.js get imported
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  // Create top level provider with the state store
+  // This allows us to access our state everywhere
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
